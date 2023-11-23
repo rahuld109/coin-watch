5efacd32068de14a3f9729d54edd83e38b3739de
Rahul Dogra <rahuldogra1998@gmail.com>
2023-11-23 18:30:35 +0530

    fix: nav list item padding

--- components/Shared/Navbar/NavigationItems.tsx
+++ components/Shared/Navbar/NavigationItems.tsx
@@ -95,13 +95,13 @@ const ListItem = React.forwardRef<
         <a
           ref={ref}
           className={cn(
-            'block select-none space-y-1 rounded-md py-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
+            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
             className
           )}
           {...props}
         >
           <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="text-sm leading-snug line-clamp-2 text-muted-foreground">
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
         </a>
