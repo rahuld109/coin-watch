d867448699bfa7b80d2f0c0237b38461f1a256e5
Rahul Dogra <rahuldogra1998@gmail.com>
2024-01-01 11:25:38 +0530

    ci: add semantic release

---
+++ release.config.js
@@ -0,0 +1,24 @@
+const config = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'docs/CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
+
+module.exports = config;
